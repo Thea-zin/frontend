b5e00d13a3fc6144d8127e75d79d54ee8d4aced8
Thea-zin <bansothea20@kit.edu.kh>
2023-01-03 14:21:17 +0700

    header work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PartialsComponent } from './components/partials/partials.component';
-import { HeaderComponent } from './components/partials/header/header.component';
+import { HeaderComponent } from './component/partial/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PartialsComponent,
-    HeaderComponent
+    HeaderComponent,
+   
   ],
   imports: [
     BrowserModule,
